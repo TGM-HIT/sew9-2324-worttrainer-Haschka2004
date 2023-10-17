a045f12f3e4feec37336b30e42a56c4cc6733104
Haschka2004 <ahaschka@student.tgm.ac.at>
2023-10-17 21:46:37 +0200

    implementierung der Gradle dependencies und die Implementierung vom UML (Code von Astah exportiert).

--- WortTrainer/build.gradle.kts
+++ WortTrainer/build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+dependencies {
+
+    // https://mvnrepository.com/artifact/com.jayway.jsonpath/json-path
+    implementation("com.jayway.jsonpath:json-path:2.8.0")
+
+
+}
 
 tasks.test {
     useJUnitPlatform()
