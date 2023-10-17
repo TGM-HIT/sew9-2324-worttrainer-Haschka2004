13e61ab76dd5f6d212e74384037a9f69f1cb02b1
Haschka2004 <ahaschka@student.tgm.ac.at>
2023-10-17 22:12:16 +0200

    Rechtsschreibtrainer Klasse wurde fast alles implementiert, außer die runde() und autoBefuellen(). Des Weiteren wurden Dokumentationskommentare in der Rechtschreibtrainer Klasse, als auch Wortpaar Klasse hinzugefügt.

--- WortTrainer/src/main/java/org/example/Rechtschreibtrainer.java
+++ WortTrainer/src/main/java/org/example/Rechtschreibtrainer.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Rechtschreibtrainer {
 
@@ -12,36 +13,62 @@ public class Rechtschreibtrainer {
 
 	private ArrayList<WortPaar> wortPaare;
 
-	private PersistenteSpeicherung persistenteSpeicherung;
+	public Rechtschreibtrainer(){
+		wortPaare = new ArrayList<>();
+		fehler = 0;
+		errateneWortpaare = 0;
+		speicher = new JSONPersistent();
+	}
 
+	/**
+	 * Gibt das Wortpaar bei einem gültigen Index zurück.
+	 * @param index, ist der Index des Wortpaares, welches zurückgegeben wird.
+	 * @return, returned bei einem gültigen Index das jeweilige Wortpaar oder bei ungültigem Index null.
+	 */
 	public WortPaar indexWortPaar(int index) {
+		if(index >= 0 && index <= wortPaare.size()) return wortPaare.get(index);
 		return null;
 	}
 
 	public WortPaar zufallsWortPaar() {
-		return null;
+		Random zufallsIndex = new Random();
+		return wortPaare.get(zufallsIndex.nextInt(wortPaare.size()));
 	}
 
 	public void spiel() {
 
 	}
 
+	/**
+	 * Zeigt die Statistik
+	 * @return, gibt die Menge an Fehlern und richtig erratenen zurück.
+	 */
 	public String statistik() {
-		return null;
+		return "Du hast: " + getFehler() + " und " + "richtig Erratene: " + getErrateneWortpaare() + ".";
 	}
 
+	/**
+	 * Fügt ein neues Wortpaar hinzu.
+	 * @param neuesWortPaar, das neue Wortpaar, welches hinzugefügt wird, solange keiner seiner Parameter null ist.
+	 * @return gibt true zurück, wenn es das Wortpaar gespeichert wurde und false, wenn nicht.
+	 */
 	public boolean addWortPaar(WortPaar neuesWortPaar) {
+		if(neuesWortPaar.getWort() != null && neuesWortPaar.getURL() != null){ wortPaare.add(neuesWortPaar); return true; }
 		return false;
 	}
 
+
 	public int getErrateneWortpaare() {
-		return 0;
+		return this.errateneWortpaare;
 	}
 
 	public int getFehler() {
-		return 0;
+		return this.fehler;
 	}
 
+	/**
+	 * Befüllt den Rechtschreibtrainer mit hard gecodeten Wortpaare.
+	 */
 	public void autoBefuellen() {
 
 	}
--- WortTrainer/src/main/java/org/example/WortPaar.java
+++ WortTrainer/src/main/java/org/example/WortPaar.java
@@ -1,11 +1,15 @@
 package org.example;
 
+/**
+ * @author Alexander Haschka
+ * @versin 2023-10-17
+ * In die Klasse Wortpaar wird die Url der Bilder und das zugehörige Wort, welches die Kinder richtig schreiben müssen gespeichert
+ */
 public class WortPaar {
 
 	private String url;
 
 	private String wort;
-
 	public WortPaar(String url, String wort) {
 		this.url = url;
 		this.wort = wort;
